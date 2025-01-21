d22d842f2116da2faaaba5461e252f2538aa85a6
Elad Karakuli <elad.karakuli@gmail.com>
2025-01-21 12:49:18 +0200

    await data copy between sheets

--- index.ts
+++ index.ts
@@ -2,7 +2,7 @@ import type { FlatfileListener } from "@flatfile/listener";
 import { automap } from "@flatfile/plugin-automap";
 import { ExcelExtractor } from "@flatfile/plugin-xlsx-extractor";
 import { externalConstraint } from "@flatfile/plugin-constraints";
-import api, { Flatfile } from "@flatfile/api";
+import api from "@flatfile/api";
 import { mapValues } from "./utils";
 import { FlatfileRecord, bulkRecordHook } from "@flatfile/plugin-record-hook";
 
@@ -82,41 +82,49 @@ export default function (listener: FlatfileListener) {
   );
 
   listener.on("workbook:created", async (event) => {
-    const workbookId = event?.context?.workbookId;
-    const sheets = (await api.sheets.list({ workbookId })).data;
-    const copyDataSheets = sheets.filter(
-      ({ config: { metadata } }) => metadata?.dataSheetId
-    );
-    copyDataSheets.forEach(async ({ id: newSheetId, config: { metadata } }) => {
-      const dataSheetId = metadata.dataSheetId;
-      console.log("copying data from", dataSheetId, "to", newSheetId);
-      try {
-        // Fetch data from the source sheet
-        const sourceRecords = await api.records.get(dataSheetId);
+    try {
+      const workbookId = event?.context?.workbookId;
+      const sheets = (await api.sheets.list({ workbookId })).data;
+      const copyDataSheets = sheets.filter(
+        ({ config: { metadata } }) => metadata?.dataSheetId
+      );
+      await Promise.all(
+        copyDataSheets.map(async ({ id: newSheetId, config: { metadata } }) => {
+          const dataSheetId = metadata.dataSheetId;
+          console.log("copying data from", dataSheetId, "to", newSheetId);
 
-        // Copy data to the new sheet
-        if (
-          sourceRecords?.data?.records &&
-          sourceRecords.data.records.length > 0
-        ) {
-          const records = sourceRecords.data.records.map(({ values }) =>
-            mapValues(values, ({ value, messages, valid }) => ({
-              value,
-              messages,
-              valid,
-            }))
-          );
-          await api.records.insert(newSheetId, records);
+          // Fetch data from the source sheet
+          const sourceRecords = await api.records.get(dataSheetId);
           console.log(
-            `Data copied from sheet ${dataSheetId} to sheet ${newSheetId}`
+            "sourceRecords",
+            sourceRecords.data.records.length,
+            sourceRecords.data.counts
           );
-        } else {
-          console.log(`No data found in source sheet ${dataSheetId}`);
-        }
-      } catch (error) {
-        console.error(`Error copying data: ${error}`);
-      }
-    });
+
+          // Copy data to the new sheet
+          if (
+            sourceRecords?.data?.records &&
+            sourceRecords.data.records.length > 0
+          ) {
+            const records = sourceRecords.data.records.map(({ values }) =>
+              mapValues(values, ({ value, messages, valid }) => ({
+                value,
+                messages,
+                valid,
+              }))
+            );
+            await api.records.insert(newSheetId, records);
+            console.log(
+              `Data copied from sheet ${dataSheetId} to sheet ${newSheetId}`
+            );
+          } else {
+            console.error(`No data found in source sheet ${dataSheetId}`);
+          }
+        })
+      );
+    } catch (error) {
+      console.error(`Error copying data: ${error}`);
+    }
   });
 
   listener.use(
